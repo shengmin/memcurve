8860aeeedd48209220c71ea3527757b18321e559
ShengMin Zhang <me@shengmin.me>
2013-06-04 01:32:00 -0400

    add terminal program

--- src/memcurve.js
+++ src/memcurve.js
@@ -1,13 +1,40 @@
-/*
- * memcurve
- * https://github.com/shengmin/memcurve
- *
- * Copyright (c) 2013 ShengMin Zhang
- * Licensed under the MIT license.
- */
-
 'use strict';
 
-exports.awesome = function() {
-  return 'awesome';
-};
+var red   = '\u001b[31m';
+var green = '\u001b[32m';
+var blue  = '\u001b[34m';
+var reset = '\u001b[0m';
+
+var StudySet = require('./StudySet');
+var studySet = StudySet.fromYaml(process.argv[2]);
+
+function printQuestion() {
+  if (studySet.hasMore()) {
+    console.log(blue + 'Question: ' + studySet.getQuestion() + reset);
+    process.stdin.once('data', onData);
+  } else {
+    console.log(green + 'Done :)' + reset);
+    process.stdin.pause();
+  }
+}
+
+function onData(data) {
+  var answer = studySet.getAnswer();
+  var color = '';
+
+  if (data.charAt(0) == 'y') {
+    studySet.markRight();
+    color = green;
+  } else {
+    studySet.markWrong();
+    color = red;
+  }
+  console.log(color + 'Answer: ' + answer + reset);
+  console.log('-----------------------');
+  printQuestion();
+}
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+printQuestion();
